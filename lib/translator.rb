445475a2c2e604e5a3042c198a9c0c0f93cee0b9
jrambold <jrambold@gmail.com>
2018-01-31 10:23:02 -0700

    eng>morse, file eng>morse, morse>eng added

--- lib/translator.rb
+++ lib/translator.rb
@@ -40,4 +40,35 @@ class Translator
                     "0" => "-----",
                     " " => " "}
   end
+
+  def eng_to_morse(phrase)
+    morse = ""
+    phrase.each_char do |phrase|
+      morse += @dictionary[phrase.downcase]
+    end
+    morse
+  end
+
+  def from_file(filename)
+    phrase = ""
+    phrase_file = IO.readlines(filename)
+    phrase_file.each do |phrase_file|
+      phrase += phrase_file.chomp.downcase
+    end
+    eng_to_morse(phrase)
+  end
+
+  def morse_to_eng(phrase)
+    eng = ""
+    phrase = phrase.split("  ")
+    phrase.each do |phrase|
+      word = phrase.split(" ")
+      word.each do |word|
+        eng += @dictionary.key(word)
+      end
+      eng += " "
+    end
+    eng.rstrip
+  end
+
 end
--- test/translator_test.rb
+++ test/translator_test.rb
@@ -1,1 +1,31 @@
-# YOUR TESTS GO IN HERE!
+require 'minitest'
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/translator'
+
+class Test_Translator < Minitest::Test
+  def setup
+    @translator = Translator.new()
+  end
+
+  def test_translator_exists
+    assert_instance_of Translator, @translator
+  end
+
+  def test_eng_to_morse
+    assert_equal @translator.eng_to_morse("hello world"), "......-...-..--- .-----.-..-..-.."
+  end
+
+  def test_case_independence
+    assert_equal @translator.eng_to_morse("Hello World"), "......-...-..--- .-----.-..-..-.."
+    assert_equal @translator.eng_to_morse("There are 3 ships"), "-......-.. .-.-.. ...-- ..........--...."
+  end
+
+  def test_from_file
+    assert_equal @translator.from_file("./test/input.txt"), ".. .--- ..-. .- ..-....-..."
+  end
+
+  def test_morse_to_eng
+    assert_equal @translator.morse_to_eng(".... . .-.. .-.. ---  .-- --- .-. .-.. -.."), "hello world"
+  end
+end
